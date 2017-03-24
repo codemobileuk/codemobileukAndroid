dae984d889d58bd2314d3b99ebc1e48797396bc9
greg <greg@footsqueek.co.uk>
2017-03-24 11:13:35 +0000

    Removed unused menu item

--- app/src/main/java/com/codemobile/footsqueek/codemobile/activities/LaunchActivity.java
+++ app/src/main/java/com/codemobile/footsqueek/codemobile/activities/LaunchActivity.java
@@ -142,9 +142,8 @@ public class LaunchActivity extends AppCompatActivity implements NavigationView.
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+
+
 
         return super.onOptionsItemSelected(item);
     }
